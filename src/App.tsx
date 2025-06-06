e700b493b42b79ccb7945e779e1cd8a08a64ca2a
EUNCHAE <2snha@naver.com>
2025-06-06 17:17:34 +0900

    fix: Notice route

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App: React.FC = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/notice" element={<Notice />} />
+                    <Route path="/notice/:id" element={<Notice />} />
                     <Route path="/project" element={<Project />} />
                 </Routes>
             </Layout>
