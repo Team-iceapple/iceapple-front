66a848ebaabbb1dbdbb0b6506405fb1cf0e96e02
EUNCHAE <2snha@naver.com>
2025-08-08 15:56:39 +0900

    chore: 넘버패드 css 조정

--- src/components/NumberPad/NumberPad.tsx
+++ src/components/NumberPad/NumberPad.tsx
@@ -1,4 +1,3 @@
-// components/NumberPad.tsx
 import styles from "./NumberPad.module.css";
 
 export default function NumberPad({
@@ -11,25 +10,31 @@ export default function NumberPad({
     maxLength?: number;
 }) {
     const handleClick = (num: string) => {
-        if (value.length < maxLength) {
-            setValue(value + num);
-        }
+        if (value.length < maxLength) setValue(value + num);
     };
 
-    const handleBackspace = () => {
-        setValue(value.slice(0, -1));
-    };
+    const handleBackspace = () => setValue(value.slice(0, -1));
 
     return (
         <div className={styles.padContainer}>
             <div className={styles.padGrid}>
-                {[..."123456789", "0"].map((n) => (
-                    <button key={n} className={styles.padBtn} onClick={() => handleClick(n)}>
+                {[
+                    "1", "2", "3",
+                    "4", "5", "6",
+                    "7", "8", "9",
+                    "","0",""
+                ].map((n) => (
+                    <button
+                        key={n}
+                        className={styles.padBtn}
+                        onClick={() => handleClick(n)}
+                    >
                         {n}
                     </button>
                 ))}
+
                 <button
-                    className={`${styles.padBtn} ${styles.backspace}`} // ✅ 수정된 부분
+                    className={`${styles.padBtn} ${styles.backspace}`}
                     onClick={handleBackspace}
                 >
                     ⌫
