f5a9b018466fc876e1c7a11bdcf8bdc982d5f258
EUNCHAE <2snha@naver.com>
2025-08-08 15:58:27 +0900

    fix: 강의실 데이터 수정

--- src/data/rooms.ts
+++ src/data/rooms.ts
@@ -1,17 +1,17 @@
 export const rooms = [
     {
         id: "capstone",
-        name: "캡스톤실",
+        name: "N5-506",
         description: "LED TV 미러링, 칠판, 4인 테이블\n해당 회의실은 선착순 배정입니다.",
     },
     {
         id: "lecture",
-        name: "강의실",
+        name: "N5-504",
         description: "LED TV 미러링, 전자칠판, 30인",
     },
     {
         id: "seminar",
-        name: "세미나실",
+        name: "N5-511",
         description: "LED TV 미러링, 칠판, 7인",
     },
 ];
